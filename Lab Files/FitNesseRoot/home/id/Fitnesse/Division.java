97cd4f93cccbb55c315a280607ee3c111b492517
Kevin Eng <k.eng2991@gmail.com>
2013-06-23 16:39:18 -0700

    Completed

--- Lab Files/FitNesseRoot/home/id/Fitnesse/Division.java
+++ Lab Files/FitNesseRoot/home/id/Fitnesse/Division.java
@@ -1,6 +1,6 @@
 public class Division {
 private double numerator, denominator;
-public void setFirstNumber(double firstno) {this.numerator = firstno;}
-public void setSecondNumber(double secondno) {this.denominator = secondno;}
-public double Result() {return numerator / denominator;}
+public void setNumerator(double firstno) {this.numerator = firstno;}
+public void setDenominator(double secondno) {this.denominator = secondno;}
+public double Quotient() {return numerator / denominator;}
 }
