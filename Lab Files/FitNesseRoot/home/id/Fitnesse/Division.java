66e2c62c573cd82d499857aec5071f0ff78cc733
Kevin Eng <k.eng2991@gmail.com>
2013-06-23 15:43:29 -0700

    Unloaded Files

---
+++ Lab Files/FitNesseRoot/home/id/Fitnesse/Division.java
@@ -0,0 +1,6 @@
+public class Division {
+private double numerator, denominator;
+public void setFirstNumber(double firstno) {this.numerator = firstno;}
+public void setSecondNumber(double secondno) {this.denominator = secondno;}
+public double Result() {return numerator / denominator;}
+}
